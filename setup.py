844f81e3491e5236b82c0322439f13790c34ffea
theSage21 <arjoonn.94@gmail.com>
2015-08-27 14:50:50 +0530

    Setup.py long_desc update

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup
 import openjudge
-import markdown2
 
 version = '.'.join(map(str, openjudge.__version__))
-with open('README.md', 'r') as f:
-    long_desc = f.read()
-    long_desc = markdown2.markdown(long_desc)
+long_desc = '''
+Openjudge is a LAN based programming judge implementation in Python3.
+
+Features
+
+    - Multi language support(Py2, Py3, C, C++, Java)
+    - Scalable
+    - Interface independent
+    - New languages can be added as and when required
+    - Complete control via interface, ie. Django admin for default interface
+    - Logging
+    - Tracebacks
+    - Timeouts
+
+
+The default interface is available at --http://theSage21.github.io/judge-interface/
+'''
 
 setup(name='openjudge',
       version=version,
