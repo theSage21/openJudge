4714dd9c8213eb20d79dc4b41616e43f3ec1c9cf
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-05-07 23:43:15 +0530

    skeleton views

--- openjudge/contests/views.py
+++ openjudge/contests/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from contests import models as M
 
-# Create your views here.
+def contests(request):
+    template, C = 'contests/home.html', dict()
+    return render(request, template, C)
+
+
+def contest_home(request, contest_pk):
+    template, C = 'contests/contest.html', dict()
+    return render(request, template, C)
+
+
+def contest_leader(request, contest_pk):
+    template, C = 'contests/leader.html', dict()
+    return render(request, template, C)
+
+
+def question(request, contest_pk, question_pk):
+    template, C = 'contests/question.html', dict()
+    return render(request, template, C)
