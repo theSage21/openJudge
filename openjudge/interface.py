170c5103d0f0d6416730e08bb212f18f78cd5c23
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-05-23 17:09:15 +0530

    ability to create attempt

--- openjudge/interface.py
+++ openjudge/interface.py
@@ -20,6 +20,31 @@ def question_display(pk):
     return tools.render('question.html', {'statement': q['statement']})
 
 
+@app.post('/attempt')
+def question_attempt():
+    question_pk = bottle.json['question']
+    language = bottle.json['language']
+    code = bottle.json['code']
+    message = 'Something Unexpected happened'
+    all_ok = True
+    if question_pk in contest['questions'].keys():
+        inp, out = [], []
+        for key, val in contest['questions'][question_pk]['testcases'].items():
+            inp.append(val['in'])
+            out.append(val['out'])
+    else:
+        message, all_ok = 'This question does not exist', False
+    if language in contest['wrappers'].keys():
+        wrap = contest['wrappers'][language]
+    else:
+        message, all_ok = 'This Language is not available', False
+    if all_ok:
+        att_id = tools.check_results_by_running_code(code, inp, out, wrap)
+    else:
+        att_id = None
+    return {'attempt': att_id, 'message': message}
+
+
 @app.get('/static/<path:path>')
 def static_server(path):
     root = config.static_root
--- openjudge/judge.py
+++ openjudge/judge.py
@@ -14,3 +14,8 @@ def __run_command__(command, timeout):
         result = p.returncode == 0
         stdout, stderr = p.stdout, p.stderr
     return result, stdout.decode(), stderr.decode()
+
+
+def check_results_by_running_code(code, inp_list, out_list, wrap):
+    attempt_id = 1
+    return attempt_id
